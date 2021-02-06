a7c830e3e6d2574d989ee3b26de2430dfe52eea6
Bechir Sibel <sibelbechir@gmail.com>
2021-02-06 18:42:25 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,47 @@
 from twilio.rest import Client
 from jokeapi import Jokes
+import csv
 
 # constants
 account_sid = ''
 auth_token = ''
-to_call = ''
-from_call = ''
+to_call = '+'
+from_call = '+'
+path = ''
 
-j = Jokes()  # Initialise the class
-joke = j.get_joke(lang='en',
-                category=['misc', 'pun', 'spooky', 'christmas'],
-                blacklist=['nsfw', 'religious', 'political', 'racist', 'sexist']) # Retrieve a random joke
+def read_numbers():
+    with open(path) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            print(f'We are calling the number: {row[0]}')
+            send_jokes(row[0])
 
-client = Client(account_sid, auth_token)
+def send_jokes(number):
+    # Create the joke
+    j = Jokes()
+    joke = j.get_joke(lang='en',
+                    category=['misc', 'pun', 'spooky', 'christmas'],
+                    blacklist=['nsfw', 'religious', 'political', 'racist', 'sexist'])
+                    # Retrieve a random joke
 
-if joke['type'] == 'single': # Print the joke
-    print(joke['joke'])
-    response = '<Response><Say>' + joke['joke'] + '</Say></Response>'
-else:
-    print(joke['setup'])
-    print(joke['delivery'])
-    response = '<Response><Say>' + joke['setup'] +  '</Say><Pause length="5"/><Say>' + joke['delivery'] + '</Say></Response>'
+    # Create the response
+    if joke['type'] == 'single':
+        print(joke['joke'])
+        response = '<Response><Say>' + joke['joke'] + '</Say></Response>'
+    else:
+        print(joke['setup'])
+        print(joke['delivery'])
+        response = '<Response><Say>' + joke['setup'] +  '</Say><Pause length="3"/><Say>' + joke['delivery'] + '</Say></Response>'
 
-call = client.calls.create(
-                        twiml=response,
-                        to=to_call,
-                        from_=from_call
-                    )
-print(call.sid)
+    # Call the person
+
+    client = Client(account_sid, auth_token)
+    call = client.calls.create(
+                            twiml=response,
+                            to=number,
+                            from_=from_call
+                        )
+    print(call.sid)
+
+if __name__ =='__main__':
+    read_numbers()
